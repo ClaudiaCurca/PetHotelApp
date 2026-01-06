192ec3ee90465d6e7b46a1fbea14f4419015d8a0
ClaudiaCurca <80842463+ClaudiaCurca@users.noreply.github.com>
2026-01-06 19:39:25 +0200

    Add Controllers and View for Animal

---
+++ PetHotelApp/Controllers/AnimalController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetHotelApp.Data;
+using PetHotelApp.Models;
+using PetHotelApp.Repository;
+
+namespace PetHotelApp.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private AnimalRepository _repository;
+        public AnimalController(ApplicationDbContext dbContext)
+        {
+            _repository = new AnimalRepository(dbContext);
+        }
+
+        // GET: AnimalController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AnimalController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AnimalController/Create
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: AnimalController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                AnimalModel model = new AnimalModel();
+                var task = TryUpdateModelAsync(model);
+                if (task.Result)
+                {
+                    _repository.CreateAnimal(model);
+                }
+                return View("Create");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: AnimalController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AnimalController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnimalController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AnimalController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PetHotelApp/Controllers/RoomAllocationController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetHotelApp.Controllers
+{
+    public class RoomAllocationController : Controller
+    {
+        // GET: RoomAllocationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RoomAllocationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RoomAllocationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoomAllocationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomAllocationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomAllocationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomAllocationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomAllocationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
